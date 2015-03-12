9e17de1e7aa9fc91b2f83a5f85973dada60f5b5c
lizzyburl <lizzyburl@gmail.com>
2015-03-12 11:46:34 -0400

    Can dynamically assign bins

--- Part2/GetMFCC.m
+++ Part2/GetMFCC.m
@@ -10,65 +10,73 @@ function [ ] = GetMFCC( filepath )
     % Shift of 160 = 10 ms
     winsize=400;
     shift=160;
-    c=1;    
+    c=1;  
+    
+    lower = 30;
+    upper = 8000;
+    
+    M = 24;
+    % First, let's dynamically build our filter bank based on the lower
+    % frequency and the upper frequency.
+    bins = GetBins(lower, upper, M);
+    fprintf('%g\n', bins);
     % Create the hamming window
     h=hamming(winsize);
     % For each window
     for i=1:shift:length(x)-winsize
         % Take the fft
         X = fft(x(i:i+winsize-1).*h,winsize);
-        L(:,c) = log(real(X).^2+imag(X).^2);
-        c=c+1;
+        
     end
 
-    % do rescaling (4)
-    % find the floor of L, reset to be 0
-    mn=min(min(L));
-    L=L-mn;
-
-    % find the max of L.  Map L to a number between 0 and 128 (at first)
-    % and then subtract 50 (-50 to 78)
-    mx=max(max(L));
-    L=floor(L/mx*128)-50;
-
-    % map all numbers below 1 to 1, all numbers above 64 to 64
-    L(find(L<1))=1;
-    L(find(L>64))=64;
     pause;
 
 end
 
+function [bins] = GetBins(lower, upper, M)
+    lower_mel = B(lower);
+    upper_mel = B(upper);
+    bins_in_mel = zeros(1, M+1);
+    bins_in_mel(1) = lower_mel;
+    bins_in_mel(M+1) = upper_mel;
+    bin_size = (upper_mel - lower_mel)/(M);
+    for i = 2:1:M
+        bins_in_mel(i) = bins_in_mel(i-1) + bin_size;
+    end
+    bins = Binv(bins_in_mel);
+end
+
 function [b] = B(f)
-    b = 1125*log(1 + f/7000);
+    b = 1125*log(1 + f/700);
 end
 
 function [b_inv] = Binv(f)
-    b_inv = 700*exp(f/1125-1)
+    b_inv = 700*(exp(f/1125)-1);
 end
 
 % N = number of points in FFT
 % fl = Lower frequency of the filterbank
 % fh = High frequency of the filterbank
-function [f] = F(m)
+function [f] = F(m, M, bins)
     % M = # of filters (try 24 - 40)
     M = 24;
     % Fs = Sampling frequency (16,000)
     Fs = 16000;
     % N = The number of frequency bins)
     N = 400;
-    % fl = 20 Hz. The low point of human hearing
-    fl = 20
-    % fh = 16000 Hz. Also our sampling frequency
-    fh = 16000
+    fl = bins(m);
+    fh = bins(m+1);
     if m == 0
         f = 0;
+    elseif m == M
+        f = N/Fs
     else
         % This might not be right...
         f = N/Fs * Binv(B(fl)+m*((B(fh)-B(fl))/(M+1)));
     end
 end
 
-function [h] = H(k, m)
-    if (k < f(m-1))
+function [h] = H(k, m, M, bins)
+    if (k < F(m-1, M, bins))
     end
 end
--- Part2/part2.m
+++ Part2/part2.m
@@ -4,31 +4,10 @@
 
 clear;
 
-<<<<<<< HEAD
-=======
-% 16,000 Hz means that 1 ms = 16 samples
-% 25 ms = 400 samples
-for soundFile = 1:10
-    [x, fs, nbits] = wavread(sprintf('rex%d.wav',soundFile));
-    timeSteps = size(x);
-   
-    winsize=400;
-    shift=160;
-    c=1;
-    clear L;
-    h=hamming(winsize);
-
-    for i=1:shift:length(x)-winsize
-         X=fft(x(i:i+winsize-1).*h,winsize);
-         L(:,c)=log(real(X).^2+imag(X).^2);
-         c=c+1;
-    end
->>>>>>> origin/master
-
-
 % 16,000 Hz means that 1 ms = 16 samples
 % 25 ms = 400 samples
 for soundFile = 1:9
     GetMFCC(sprintf('%da.wav',soundFile));
     GetMFCC(sprintf('%db.wav',soundFile));
+    pause;
 end
